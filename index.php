fac3f833bd4bae7ffc01cd6e2319664a0373c211
Manasi <manasighutukade@gmail.com>
2021-12-03 23:45:09 +0530

    final🍕

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+if(isset($_POST['name']) ){
+
+
+$server= "localhost";
+
+$username = "root";
+$password ="";
+
+$con = mysqli_connect($server ,$username , $password);
+
+if(!$con)
+{
+    die("connection failed due to" . mysqli_connect_error());
+}
+//echo "Success connecting to the database";
+ $name=$_POST['name'];
+ $email=$_POST['email'];
+ $phone=$_POST['phone'];
+ $message=$_POST['message'];
+ $crust=$_POST['crust'];
+ $sauce=$_POST['sauce'];
+ $cheese=$_POST['cheese'];
+ $toppings=$_POST['toppings'];
+ $sql = "INSERT INTO `pizzaregisteration`.`pizzario` ( `name`, `email`, `phone`, `message`, `crust`, 
+`sauce`, `cheese`, `toppings`, `date`) VALUES ('$name', '$email', 
+'$phone', '$message', '$crust', '$sauce', '$cheese', '$toppings', CURRENT_TIMESTAMP);";
+
+echo $sql;
+
+if($con->query($sql) == TRUE){
+    echo "successfully inserted";
+}
+else{
+    echo "ERROR : $sql <br> $con->error";
+}
+$con->close();
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
