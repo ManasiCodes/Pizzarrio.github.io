626203c4e5c847bab35d199dabbba588ca534c7c
Manasi <manasighutukade@gmail.com>
2021-12-03 22:40:20 +0530

    minor

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$server= "localhost";
+
+$username = "root";
+$password ="";
+
+$con = mysqli_connect($server ,$username , $password);
+
+if(!$con)
+{
+    die("connection failed due to" . mysqli_connect_error());
+}
+echo "Success connecting to the database";
+?>
