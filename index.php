ee5d1ecc2c6a76564cb0c22acca6a8d36a306711
Manasi <manasighutukade@gmail.com>
2021-12-03 23:09:11 +0530

    minor

--- index.php
+++ index.php
@@ -10,5 +10,18 @@ if(!$con)
 {
     die("connection failed due to" . mysqli_connect_error());
 }
-echo "Success connecting to the database";
+//echo "Success connecting to the database";
+ $name=$_POST['name'];
+ $email=$_POST['email'];
+ $phone=$_POST['phone'];
+ $message=$_POST['message'];
+ $crust=$_POST['crust'];
+ $sauce=$_POST['sauce'];
+ $cheese=$_POST['cheese'];
+ $toppings=$_POST['toppings'];
+ $sql = "INSERT INTO `pizzario` ( `name`, `email`, `phone`, `message`, `crust`, 
+`sauce`, `cheese`, `toppings`, `date`) VALUES ('$name', '$email', 
+'$phone', '$message', '$crust', '$sauce', '$cheese', '$toppings', CURRENT_TIMESTAMP);";
+
+echo $sql;
 ?>
